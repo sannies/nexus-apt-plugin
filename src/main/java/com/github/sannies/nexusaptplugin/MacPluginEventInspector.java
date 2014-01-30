2ad2e5e851273913f811b316c0dd6e8341170b4d
Dominik Menzi <dominik.menzi@inventage.com>
2014-01-30 10:53:48 +0100

    Rewrite event inspector for new events system

--- src/main/java/com/github/sannies/nexusaptplugin/MacPluginEventInspector.java
+++ src/main/java/com/github/sannies/nexusaptplugin/MacPluginEventInspector.java
@@ -5,18 +5,17 @@ import java.util.Map;
 
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.inject.Singleton;
 
 import org.slf4j.Logger;
+import org.sonatype.nexus.events.EventSubscriber;
 import org.sonatype.nexus.proxy.AccessDeniedException;
 import org.sonatype.nexus.proxy.IllegalOperationException;
 import org.sonatype.nexus.proxy.ItemNotFoundException;
 import org.sonatype.nexus.proxy.RepositoryNotAvailableException;
 import org.sonatype.nexus.proxy.ResourceStoreRequest;
 import org.sonatype.nexus.proxy.StorageException;
-import org.sonatype.nexus.proxy.events.EventInspector;
-import org.sonatype.nexus.proxy.events.RepositoryEvent;
 import org.sonatype.nexus.proxy.events.RepositoryEventLocalStatusChanged;
-import org.sonatype.nexus.proxy.events.RepositoryItemEvent;
 import org.sonatype.nexus.proxy.events.RepositoryItemEventDelete;
 import org.sonatype.nexus.proxy.events.RepositoryItemEventStore;
 import org.sonatype.nexus.proxy.events.RepositoryRegistryEventAdd;
@@ -30,10 +29,9 @@ import org.sonatype.nexus.proxy.repository.LocalStatus;
 import org.sonatype.nexus.proxy.repository.ProxyRepository;
 import org.sonatype.nexus.proxy.repository.Repository;
 import org.sonatype.nexus.proxy.storage.UnsupportedStorageOperationException;
-import org.sonatype.plexus.appevents.Event;
 
-import com.github.sannies.nexusaptplugin.sign.AptSigningConfiguration;
 import com.google.common.collect.ImmutableMap;
+import com.google.common.eventbus.Subscribe;
 
 /**
  * EventInspector that listens to registry events, repo addition and removal, and simply "hooks" in the generated
@@ -41,8 +39,10 @@ import com.google.common.collect.ImmutableMap;
  *
  * @author cstamas
  */
+@Singleton
+@Named("deb")
 public class MacPluginEventInspector
-        implements EventInspector {
+        implements EventSubscriber {
     public static final String PACKAGES_ARCHETYPE_PATH = "/Packages";
     public static final String PACKAGES_GZ_ARCHETYPE_PATH = "/Packages.gz";
     public static final String RELEASE_ARCHETYPE_PATH = "/Release";
@@ -58,106 +58,88 @@ public class MacPluginEventInspector
                     .put(PUBLIC_KEY_ARCHETYPE_PATH, SignKeyContentGenerator.ID)
                     .build();
 
-    @Inject
-    private Logger logger;
+    private final Logger logger;
+
+    private final ContentClass maven2ContentClass;
 
     @Inject
-    @Named("maven2")
-    private ContentClass maven2ContentClass;
-
-    @Inject
-    private AptSigningConfiguration signingConfiguration;
-
-    public boolean accepts(Event<?> evt) {
-        Repository repository;
-        if (evt instanceof RepositoryItemEventStore
-                || evt instanceof RepositoryItemEventDelete
-                || evt instanceof RepositoryEventLocalStatusChanged) {
-            repository = ((RepositoryEvent) evt).getRepository();
-        }
-        else if (evt instanceof RepositoryRegistryEventAdd) {
-            repository = ((RepositoryRegistryEventAdd) evt).getRepository();
-        }
-        else {
-            // Not interested in this event type
-            return false;
-        }
-
-        // Check that the repository is compatible
-        if (!(maven2ContentClass.isCompatible(repository.getRepositoryContentClass())
-                && (repository.getRepositoryKind().isFacetAvailable(HostedRepository.class)
-                || repository.getRepositoryKind().isFacetAvailable(ProxyRepository.class) || repository.getRepositoryKind().isFacetAvailable(
-                GroupRepository.class)))) {
-            // Not compatible with this repository
-            return false;
-        }
-
-        // Check that the repository is in service
-        if (evt instanceof RepositoryEventLocalStatusChanged
-                && !LocalStatus.IN_SERVICE.equals(((RepositoryEventLocalStatusChanged) evt).getNewLocalStatus())) {
-            // Repository is not local
-            return false;
-        }
-
-        // If we haven't returned anything yet we're interested in the event
-        return true;
+    public MacPluginEventInspector(Logger logger, @Named("maven2") ContentClass maven2ContentClass) {
+        this.logger = logger;
+        this.maven2ContentClass = maven2ContentClass;
     }
 
-    public void inspect(Event<?> evt) {
-        Repository repository = null;
+    @Subscribe
+    public void onRepositoryRegistryEventAdd(RepositoryRegistryEventAdd evt) {
+        installArcheTypeCatalogIfCompatible(evt.getRepository());
+    }
 
-        if (evt instanceof RepositoryRegistryEventAdd) {
-            repository = ((RepositoryRegistryEventAdd) evt).getRepository();
-        }
-        else if (evt instanceof RepositoryEvent) {
-            repository = ((RepositoryEvent) evt).getRepository();
-        }
-        else {
-            // huh?
+    @Subscribe
+    public void onRepositoryEventLocalStatusChanged(RepositoryEventLocalStatusChanged evt) {
+        if (!isLocalStatusInService(evt)) {
             return;
         }
+        installArcheTypeCatalogIfCompatible(evt.getRepository());
+    }
 
-        if (evt instanceof RepositoryRegistryEventAdd
-                || evt instanceof RepositoryEventLocalStatusChanged) {
-            installArchetypeCatalog(repository);
+    private boolean isLocalStatusInService(RepositoryEventLocalStatusChanged evt) {
+        return LocalStatus.IN_SERVICE.equals(evt.getNewLocalStatus());
+    }
+
+    private void installArcheTypeCatalogIfCompatible(Repository repository) {
+        installArchetypeCatalog(repository);
+    }
+
+    @Subscribe
+    public void onRepositoryItemEventStore(RepositoryItemEventStore evt) {
+        updateDebItemInRepository(evt.getRepository(), evt.getItem());
+    }
+
+    @Subscribe
+    public void onRepositoryItemEventDelete(RepositoryItemEventDelete evt) {
+        updateDebItemInRepository(evt.getRepository(), evt.getItem());
+    }
+
+    private void updateDebItemInRepository(Repository repository, StorageItem item) {
+        if (item.getName().toLowerCase().endsWith(".deb")) {
+            updateFileModificationDate(repository);
         }
-        else if (evt instanceof RepositoryItemEvent) {
-            StorageItem item = ((RepositoryItemEvent) evt).getItem();
-            if (item.getName().toLowerCase().endsWith(".deb")) {
-                updateFileModificationDate(repository);
-            }
-        }
+    }
+
+    private boolean isRepositoryCompatible(Repository repository) {
+        // check is it a maven2 content, and either a "hosted", "proxy" or "group" repository
+        return maven2ContentClass.isCompatible(repository.getRepositoryContentClass())
+                && (repository.getRepositoryKind().isFacetAvailable(HostedRepository.class)
+                || repository.getRepositoryKind().isFacetAvailable(ProxyRepository.class) || repository.getRepositoryKind().isFacetAvailable(
+                GroupRepository.class));
     }
 
     public void installArchetypeCatalog(Repository repository) {
-        // check is it a maven2 content, and either a "hosted", "proxy" or "group" repository
-        if (maven2ContentClass.isCompatible(repository.getRepositoryContentClass())
-                && (repository.getRepositoryKind().isFacetAvailable(HostedRepository.class)
-                || repository.getRepositoryKind().isFacetAvailable(ProxyRepository.class) || repository.getRepositoryKind().isFacetAvailable(
-                GroupRepository.class))) {
-            // new repo added or enabled, "install" the archetype catalogs
-            try {
-                for (Map.Entry<String, String> entry : PATHS_TO_GENERATOR_MAP.entrySet()) {
-                    // Packages.gz
-                    DefaultStorageFileItem file =
-                            new DefaultStorageFileItem(repository,
-                                    new ResourceStoreRequest(entry.getKey()), true, false,
-                                    new StringContentLocator(entry.getValue()));
-                    file.setContentGeneratorId(entry.getValue());
-                    repository.storeItem(false, file);
-                }
+        if (!isRepositoryCompatible(repository)) {
+            return;
+        }
+
+        // new repo added or enabled, "install" the archetype catalogs
+        try {
+            for (Map.Entry<String, String> entry : PATHS_TO_GENERATOR_MAP.entrySet()) {
+                // Packages.gz
+                DefaultStorageFileItem file =
+                        new DefaultStorageFileItem(repository,
+                                new ResourceStoreRequest(entry.getKey()), true, false,
+                                new StringContentLocator(entry.getValue()));
+                file.setContentGeneratorId(entry.getValue());
+                repository.storeItem(false, file);
             }
-            catch (RepositoryNotAvailableException e) {
-                logger.info("Unable to install the generated archetype catalog, repository \""
-                        + e.getRepository().getId() + "\" is out of service.");
+        }
+        catch (RepositoryNotAvailableException e) {
+            logger.info("Unable to install the generated archetype catalog, repository \""
+                    + e.getRepository().getId() + "\" is out of service.");
+        }
+        catch (Exception e) {
+            if (logger.isDebugEnabled()) {
+                logger.info("Unable to install the generated archetype catalog!", e);
             }
-            catch (Exception e) {
-                if (logger.isDebugEnabled()) {
-                    logger.info("Unable to install the generated archetype catalog!", e);
-                }
-                else {
-                    logger.info("Unable to install the generated archetype catalog:" + e.getMessage());
-                }
+            else {
+                logger.info("Unable to install the generated archetype catalog:" + e.getMessage());
             }
         }
     }
@@ -168,6 +150,10 @@ public class MacPluginEventInspector
      * @param repository The repository to update files in
      */
     public void updateFileModificationDate(Repository repository) {
+        if (!isRepositoryCompatible(repository)) {
+            return;
+        }
+
         for (String path : PATHS_TO_GENERATOR_MAP.keySet()) {
             try {
                 // Retrieve the item, update the modification time and save it
@@ -176,19 +162,19 @@ public class MacPluginEventInspector
                 repository.storeItem(false, item);
             }
             catch (StorageException e) {
-                logger.error("e");
+                logger.error("e", e);
             }
             catch (AccessDeniedException e) {
-                logger.error("e");
+                logger.error("e", e);
             }
             catch (ItemNotFoundException e) {
-                logger.error("e");
+                logger.error("e", e);
             }
             catch (IllegalOperationException e) {
-                logger.error("e");
+                logger.error("e", e);
             }
             catch (UnsupportedStorageOperationException e) {
-                logger.error("e");
+                logger.error("e", e);
             }
         }
     }
